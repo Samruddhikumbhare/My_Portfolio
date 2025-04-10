e462d64aaf88d8d3da696ebdce5da1725dea2eea
root <root@ip-172-31-20-4.ap-southeast-1.compute.internal>
2025-04-10 10:47:17 +0000

    2nd update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    name = "Samruddhi Nikhare"
+    name = "Samruddhi Kumbhare"
     projects = [
         {
             "title": "CI/CD pipeline with GitHub webhook", 
